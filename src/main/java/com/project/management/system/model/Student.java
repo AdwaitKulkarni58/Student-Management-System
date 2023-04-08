35417cc0063c7a94fab010437691a382c17f1364
Adwait <adwait.kul.2018@gmail.com>
2023-04-08 16:02:55 -0700

    set table column names

--- src/main/java/com/project/management/system/model/Student.java
+++ src/main/java/com/project/management/system/model/Student.java
@@ -1,5 +1,6 @@
 package com.project.management.system.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +15,13 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "first_name", nullable = false)
 	private String firstName;
 
+	@Column(name = "last_name")
 	private String lastName;
 
+	@Column(name = "email")
 	private String email;
 
 	public Student() {
