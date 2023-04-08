da380652f1fcfaaa3a875319b5313fae9dd29a61
Adwait <adwait.kul.2018@gmail.com>
2023-04-08 16:43:09 -0700

    rename service class to interface and annotate impl with service

--- src/main/java/com/project/management/system/service/StudentService.java
+++ src/main/java/com/project/management/system/service/StudentService.java
@@ -1,14 +1,5 @@
 package com.project.management.system.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.project.management.system.config.ApplicationProperties;
-
-@Service
-public class StudentService {
-
-	@Autowired
-	private ApplicationProperties applicationProperties;
+public interface StudentService {
 
 }
--- src/main/java/com/project/management/system/service/impl/StudentServiceImpl.java
+++ src/main/java/com/project/management/system/service/impl/StudentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.project.management.system.service.impl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentServiceImpl {
 
 }
