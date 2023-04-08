2452b047a0f6df763c12f9e39fa7d3a63091ac83
Adwait <adwait.kul.2018@gmail.com>
2023-04-08 16:46:49 -0700

    stubs for implementation methods

--- src/main/java/com/project/management/system/service/impl/StudentServiceImpl.java
+++ src/main/java/com/project/management/system/service/impl/StudentServiceImpl.java
@@ -1,8 +1,43 @@
 package com.project.management.system.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.project.management.system.model.Student;
+import com.project.management.system.service.StudentService;
+
 @Service
-public class StudentServiceImpl {
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student saveStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudentById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteStudentById(Long id) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
