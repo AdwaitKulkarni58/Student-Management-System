a095d40592b7f2122cc58e91226e2e2deb0dc31f
Adwait <adwait.kul.2018@gmail.com>
2023-04-08 16:46:06 -0700

    add service interface methods

--- src/main/java/com/project/management/system/service/StudentService.java
+++ src/main/java/com/project/management/system/service/StudentService.java
@@ -1,5 +1,19 @@
 package com.project.management.system.service;
 
+import java.util.List;
+
+import com.project.management.system.model.Student;
+
 public interface StudentService {
+
+	public List<Student> getAllStudents();
+
+	public Student saveStudent(Student student);
+
+	public Student getStudentById(Long id);
+
+	public Student updateStudent(Student student);
+
+	public void deleteStudentById(Long id);
 
 }
