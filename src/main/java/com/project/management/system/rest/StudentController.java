435a5d8212e2814a8a4b668e9874205a425b5b31
Adwait <adwait.kul.2018@gmail.com>
2023-04-08 17:00:21 -0700

    create get mapping for listing all students

--- src/main/java/com/project/management/system/rest/StudentController.java
+++ src/main/java/com/project/management/system/rest/StudentController.java
@@ -1,9 +1,24 @@
 package com.project.management.system.rest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project.management.system.service.StudentService;
 
 @Controller
 public class StudentController {
-	
+
+	private StudentService studentService;
+
+	public StudentController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
+	}
+
+	@GetMapping("/students")
+	public String listStudents() {
+		studentService.getAllStudents();
+		return null;
+	}
 
 }
