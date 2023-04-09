ec81dd9f0a5575c84a7c19acd140583d10a88d60
Adwait <adwait.kul.2018@gmail.com>
2023-04-08 18:54:21 -0700

    new get mapping for adding new student

--- src/main/java/com/project/management/system/rest/StudentController.java
+++ src/main/java/com/project/management/system/rest/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.management.system.model.Student;
 import com.project.management.system.service.StudentService;
 
 @Controller
@@ -20,6 +21,13 @@ public class StudentController {
 	public String listStudents(Model model) {
 		model.addAttribute("students", studentService.getAllStudents());
 		return "students";
+	}
+
+	@GetMapping("/students/new")
+	public String createStudent(Model model) {
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return "create_student";
 	}
 
 }
