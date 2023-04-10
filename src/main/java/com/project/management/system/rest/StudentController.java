9452c8b058cb23db5bad5848706262c789fab4f5
Adwait <adwait.kul.2018@gmail.com>
2023-04-10 16:46:48 -0700

    substituted spring fox with spring doc for spring boot 3.x

--- src/main/java/com/project/management/system/rest/StudentController.java
+++ src/main/java/com/project/management/system/rest/StudentController.java
@@ -1,16 +1,18 @@
 package com.project.management.system.rest;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.management.system.model.Student;
 import com.project.management.system.service.StudentService;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class StudentController {
 
 	private StudentService studentService;
