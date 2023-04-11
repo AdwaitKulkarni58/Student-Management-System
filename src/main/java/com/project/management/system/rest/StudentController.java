75bcd5d337b9c96068070f4fb230eb86baf5680d
Adwait <adwait.kul.2018@gmail.com>
2023-04-10 17:00:04 -0700

    add swagger support

--- src/main/java/com/project/management/system/rest/StudentController.java
+++ src/main/java/com/project/management/system/rest/StudentController.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.management.system.model.Student;
 import com.project.management.system.service.StudentService;
 
 @RestController
-@RequestMapping("/api")
 public class StudentController {
 
 	private StudentService studentService;
